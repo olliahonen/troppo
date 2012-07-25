e455f5b881a0624fd08b24103fbe16e481e4ae6b
Olli Ahonen <olli.ahonen>
2012-07-25 22:00:19 +0300

    First sound implementation

--- troppo.js
+++ troppo.js
@@ -1,11 +1,26 @@
 // Namespace
 troppo = {};
 
+troppo.audio = new Audio();
+
 troppo.initialize = function() {
 	
 	// Listen to key strokes
 	$(window).keypress(function(event) {
+		// Show the symbol
 		var inputSymbol = String.fromCharCode(event.which);
 		$('#characterDisplay').html(inputSymbol.toUpperCase() + ' ' + inputSymbol.toLowerCase());
+
+		// Play the sound
+		var audio = troppo.audio;
+		audio.pause();
+
+		var audioPath = 'audio/olli/v1/' + inputSymbol + '.wav'
+		try {
+			audio.src = audioPath;
+		} catch (exception) {
+			// Pokemon!
+		}
+		audio.play();
 	});
 };
