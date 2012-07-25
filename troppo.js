94cf41b5a0644dfa67fe699682df91b81fc0dcd4
Olli Ahonen <olli.ahonen>
2012-07-25 21:23:13 +0300

    Added comment

--- troppo.js
+++ troppo.js
@@ -3,6 +3,7 @@ troppo = {};
 
 troppo.initialize = function() {
 	
+	// Listen to key strokes
 	$(window).keypress(function(event) {
 		var inputSymbol = String.fromCharCode(event.which);
 		$('#characterDisplay').html(inputSymbol.toUpperCase() + ' ' + inputSymbol.toLowerCase());
