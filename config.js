b80db3388c279f795a5e492486d4d3d506deaff9
Olli Ahonen <olli.ahonen>
2012-07-26 22:48:09 +0300

    Reformatted for easier modification

--- config.js
+++ config.js
@@ -3,4 +3,7 @@ window.troppo = {};
 
 var troppo = window.troppo;
 
-troppo.artists = [{name: 'alvar', version: 1}, {name: 'olli', version: 1}];
+troppo.artists = [
+	{name: 'alvar', version: 1}
+	,{name: 'olli', version: 1}
+];
