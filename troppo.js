d8cb4fe3c7b2941501a4535f60010e3c406b58d4
Olli Ahonen <olli.ahonen>
2012-07-26 08:50:41 +0300

    More letters, another artist

--- troppo.js
+++ troppo.js
@@ -1,12 +1,14 @@
 // Namespace
 troppo = {};
 
-troppo.audio = new Audio();
+troppo.audio = null;
 
 troppo.initialize = function() {
+
+	troppo.audio = new Audio();
 	
 	// Listen to key strokes
-	$(window).keypress(function(event) {
+	$(window).keyup(function(event) {
 		// Show the symbol
 		var inputSymbol = String.fromCharCode(event.which);
 		$('#characterDisplay').html(inputSymbol.toUpperCase() + ' ' + inputSymbol.toLowerCase());
@@ -15,7 +17,11 @@ troppo.initialize = function() {
 		var audio = troppo.audio;
 		audio.pause();
 
-		var audioPath = 'audio/olli/v1/' + inputSymbol + '.wav'
+		var artist = 'olli';
+		if (Math.random() > 0.5) {
+			artist = 'alvar';
+		}
+		var audioPath = 'audio/' + artist + '/v1/' + inputSymbol + '.wav'
 		try {
 			audio.src = audioPath;
 		} catch (exception) {
