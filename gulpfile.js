2461cf24160474647ee1722aa82422995e00ad07
Olli Ahonen <olli.ahonen>
2015-01-15 13:15:25 +0200

    Remove Gulp

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var jshint = require('gulp-jshint');
-
-gulp.task('lintjs', function () {
-  return gulp
-    .src('*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
-});
