b97c69ca521a86735a965c000620bf2b99e821d6
Olli Ahonen <olli.ahonen>
2015-07-13 20:54:02 +0300

    Improve playback

--- src/player.js
+++ src/player.js
@@ -1,26 +1,12 @@
+var howler = require('howler');
 var SymbolView = require('./views/SymbolView.js');
 
-var audio = null;
-
 var init = function () {
-  audio = new Audio();
   SymbolView.audioPath.subscribe(playSound);
 };
 
 var playSound = function (path) {
-  if (!audio.error && !audio.ended && audio.src) {
-    // Still playing, abort
-    return;
-  }
-
-  audio.pause();
-
-  try {
-    audio.src = path;
-  } catch (exception) {
-    // Pokemon!
-  }
-  audio.play();
+  (new howler.Howl({urls: [path]})).play();
 };
 
 module.exports = {
